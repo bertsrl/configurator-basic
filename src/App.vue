125c1305896ab73225f88e0284c9694bcefe8edc
Andrei Bertalan <andrei.bertalan@360desgin.ro>
2023-07-20 17:59:20 +0300

    added license

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,7 @@ import ExperienceComponent from './components/ExperienceComponent.vue';
   <div>
     <ExperienceComponent />
   </div>
+  <div id="license" style="color: #fff; font-size: 3vw;">
+    By Bertalan Andrei-Eduard
+  </div>
 </template>
